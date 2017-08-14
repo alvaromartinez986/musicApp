025d5d9441b2c05c494bb1cde38f58d7f946b5bf
Alvaro Martinez <msae986@gmail.com>
2017-08-14 10:09:42 -0500

    Add login service

--- controllers/user.js
+++ controllers/user.js
@@ -3,63 +3,105 @@
 let bcrypt = require('bcrypt-nodejs')
 let User = require('../models/user');
 
-function pruebas(req, res){
-  res.status(200).send({
-    message: 'Proando una accion del controlador del usuario del Application rest'
-  })
+function pruebas(req, res) {
+    res.status(200).send({
+        message: 'Proando una accion del controlador del usuario del Application rest'
+    })
 }
 
-function saveUser(req, res){
-  let user = new User();
+function saveUser(req, res) {
+    let user = new User();
 
-  let params = req.body;
+    let params = req.body;
 
     console.log(params);
 
-  user.name = params.name;
-  user.surname = params.surname;
-  user.email = params.email;
-  user.role = 'ROLE_USER';
-  user.image = 'null';
+    user.name = params.name;
+    user.surname = params.surname;
+    user.email = params.email;
+    user.role = 'ROLE_USER';
+    user.image = 'null';
 
-  if(params.password){
+    if (params.password) {
 
-    bcrypt.hash(params.password, null, null, function(err, hash){
-      user.password = hash;
-      if(user.name!=null && user.surname != null && user.email!= null){
-        user.save((err, userStored)=>{
-          if(err){
-            res.status(500).send({
-              message: "Error al guardar el usuario"
-            });
-          }else{
-            if(!userStored){
-              res.status(404).send({
-                message: "No se ha registrado el usuario"
-              });
-            }else{
-              res.status(200).send({
-                user: userStored,
-                message: "Usuario creado correctamente"
-              });
+        bcrypt.hash(params.password, null, null, function(err, hash) {
+            user.password = hash;
+            if (user.name != null && user.surname != null && user.email != null) {
+                user.save((err, userStored) => {
+                    if (err) {
+                        res.status(500).send({
+                            message: "Error al guardar el usuario"
+                        });
+                    } else {
+                        if (!userStored) {
+                            res.status(404).send({
+                                message: "No se ha registrado el usuario"
+                            });
+                        } else {
+                            res.status(200).send({
+                                user: userStored,
+                                message: "Usuario creado correctamente"
+                            });
+                        }
+                    }
+                });
+            } else {
+                res.status(200).send({
+                    message: "Introduce todos los campos"
+                });
             }
-          }
         });
-      }else {
+
+    } else {
         res.status(200).send({
-          message: "Introduce todos los campos"
+            message: "Introduzca contraseña"
         });
-      }
+    }
+}
+
+function loginUser(req, res) {
+
+    let params = req.body;
+
+    let email = params.email;
+    let password = params.password;
+
+    User.findOne({
+        email: email.toLowerCase()
+    }, (err, user) => {
+        if (err) {
+            res.status(500).send({
+                message: "Error en la peticion"
+            });
+        } else {
+            if (!user) {
+                res.status(404).send({
+                    message: "El usuario no existe"
+                });
+            } else {
+                bcrypt.compare(password, user.password, (err, check) => {
+                    if (check) {
+                        if (params.gethash) {
+
+                        } else {
+                            res.status(200).send({
+                                user
+                            });
+                        }
+                    } else {
+                        res.status(404).send({
+                            message: "El usuario no ha podido loguearse"
+                        });
+                    }
+                });
+            }
+        }
     });
 
-  }else {
-    res.status(200).send({
-      message: "Introduzca contraseña"
-    });
-  }
 }
 
 module.exports = {
-  pruebas,
-  saveUser
+    pruebas,
+    saveUser,
+    loginUser
 };
--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ let api = express.Router();
 
 api.get('/probando-controlador', userController.pruebas);
 api.post('/register', userController.saveUser);
+api.post('/login', userController.loginUser);
 
 
 module.exports = api;
